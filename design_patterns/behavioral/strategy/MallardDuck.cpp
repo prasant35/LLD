16fa901a0f6bc4d09d0ae0cf270cb7588322d6ad
kprasant35 <prasantkumar.bt19ece@pec.edu.in>
2025-06-02 18:21:22 +0530

    Added Mallard duck

---
+++ design_patterns/behavioral/strategy/MallardDuck.cpp
@@ -0,0 +1,15 @@
+#include "Duck.hpp"
+#include "FlyWithWings.cpp"
+#include "Quack.cpp"
+#include <iostream>
+
+class MallardDuck : public Duck{
+public:
+    MallardDuck(){
+        flyBehavior = new FlyWithWings();
+        quackBehavior = new Quack();
+    }
+    void display() override{
+        std::cout<<"I am Mallard Duck!"<<std::endl;
+    }
+};
