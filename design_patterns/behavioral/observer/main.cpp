dc41c78d405e276a405d8fb877b901c76773851c
Prasant <kprasant35@gmail.com>
2025-06-03 17:28:52 +0530

    Added new display concrete class

--- design_patterns/behavioral/observer/main.cpp
+++ design_patterns/behavioral/observer/main.cpp
@@ -1,10 +1,12 @@
 #include "src/CurrentConditionsDisplay.cpp"
 #include "src/WeatherData.cpp"
+#include "src/statisticDisplay.cpp"
 #include <iostream>
 
 int main(){
     WeatherData weatherData;
     CurrentConditionsDisplay currentConditionsDisplay(&weatherData);
+    StatisticDisplay statisticDisplay(&weatherData);
 
     weatherData.setMeasurements(80, 65, 30.4f);
     weatherData.setMeasurements(82, 70, 29.2f);
---
+++ design_patterns/behavioral/observer/src/statisticDisplay.cpp
@@ -0,0 +1,27 @@
+#include "../include/Display.hpp"
+#include "../include/Observer.hpp"
+#include "../include/Subject.hpp"
+#include <iostream>
+
+class StatisticDisplay : public Observer, public Display{
+    float maxTemp = 0.0f;
+    float minTemp = 1000.0f;
+    Subject* weatherData;
+public:
+    StatisticDisplay(Subject* weatherData) : weatherData(weatherData) {
+        weatherData->registerObserver(this);
+    }
+
+    void update(float temperature, float humidity, float pressure) override{
+        if(temperature > maxTemp){
+            maxTemp = temperature;
+        }
+        if(temperature < minTemp){
+            minTemp = temperature;
+        }
+        display();
+    }
+    void display() override{
+        std::cout << "Max/Min temperature: " << maxTemp << "/" << minTemp << std::endl;
+    }
+};
