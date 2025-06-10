3526debc32872187d82f2da356b2963db26e6aa6
Prasant <kprasant35@gmail.com>
2025-06-10 14:47:36 +0530

    Added player and board class

--- practice/snake_game/main.cpp
+++ practice/snake_game/main.cpp
@@ -20,3 +20,40 @@ public:
         return (rand()%3 + 1) * 2;
     }
 }
+
+// Player class
+class Player{
+    int position;
+    string name;
+public:
+
+    Player(string name) : name(name), position(1) {}
+    void move(int steps){
+        position += steps;
+    }
+
+    void setPosition(int position){ // due to snake and ladder, we need to set the position of the player
+        this->position = position;
+    }
+
+    int getPosition(){
+        return position;
+    }
+}
+
+// Board class
+class Board{
+    int size;
+    unordered_map<int, int> snakes, ladders;
+public:
+    Board(int size) : size(size) {}
+    void addSnake(int start, int end){  // assuming no snake share the same start point
+        snakes[start] = end;
+    }
+
+    int checkPositionForSnake(int position){
+        if(snakes.count(position)){
+            return snakes[position];
+        }else return position;
+    }
+}
